b1ae05cfe9378add459e26d237f8bade5bdd1246
Sagar Prasad Barnwal <sagarbarnwal722@gmail.com>
2026-08-16 18:53:49 +0530

    Time: 1 ms (94.39%) | Memory: 42.4 MB (93.32%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,7 +1,7 @@
 class Solution {
     public int divide(int dividend, int divisor) {
 
-        // Handle overflow case
+        
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
@@ -25,7 +25,6 @@ class Solution {
             count = count + multiple;
         }
 
-        // Determine sign
         if ((dividend < 0) != (divisor < 0)) {
             count = -count;
         }
