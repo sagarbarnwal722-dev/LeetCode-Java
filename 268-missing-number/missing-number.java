9b1be4d282cac4b61d3c317d618a3b3e037ea73b
Sagar Prasad Barnwal <sagarbarnwal722@gmail.com>
2026-09-02 00:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (49.04%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,18 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int xor = nums.length;
+        // int xor = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
-            xor = xor ^ i ^ nums[i];
+        // for (int i = 0; i < nums.length; i++) {
+        //     xor = xor ^ i ^ nums[i];
+        // }
+
+        // return xor;
+        int len=nums.length,act=0;
+        int expect=(len*(len+1))/2;
+        for(int i:nums)
+        {
+            act+=i;
         }
-
-        return xor;
+        return expect-act;
     }
 }
