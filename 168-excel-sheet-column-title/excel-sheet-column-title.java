3fd962968bd88dc5879a8068e796edf5b658881a
Sagar Prasad Barnwal <sagarbarnwal722@gmail.com>
2026-09-02 00:09:24 +0530

    Time: 3 ms (12.58%) | Memory: 43 MB (8.40%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -4,8 +4,8 @@ class Solution {
        while(columnNumber>0)
        {
         columnNumber--;
-        int rem=columnNumber%26;
-        str=(char)('A'+rem)+str;
+        //int rem=columnNumber%26;
+        str=(char)('A'+(columnNumber%26))+str;
         columnNumber/=26;
        }
        return str; 
