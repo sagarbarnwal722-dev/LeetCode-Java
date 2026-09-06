0413461f4689afd6f1f64f92598d851d1b137140
Sagar Prasad Barnwal <sagarbarnwal722@gmail.com>
2026-09-06 23:06:33 +0530

    Time: 24 ms (28.61%) | Memory: 81.5 MB (86.01%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       int i,j;
+       Arrays.sort(nums);
+       for(i=1;i<nums.length;i++)
+       {
+        if(nums[i]==nums[i-1])
+        {
+            return true;
+        }
+       }
+       return false;
+    }
+}
